be32c87adb3761119caca4b2dab252cb93b352a6
zKXDEX <deiividdlk@gmail.com>
2023-11-21 14:45:16 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,47 +1,44 @@
 // Define the structure for a registry value
 export interface RegistryValue {
-    name: string; // The name of the registry value
-    type: REG; // The type of the registry value
-    value: string; // The value of the registry value
+    name: string 
+    type: REG
+    value: any
 }
 
-// Define the possible root keys in the registry
-export enum HKEY{
+export enum HK {
     CR = 0x80000000, // Classes root
     CU = 0x80000001, // Current user
     LM = 0x80000002, // Local machine
     U = 0x80000003,  // Users
     PD = 0x80000004, // Performance data
     CC = 0x80000005, // Current config
-    CD = 0x80000006, // Current dynamic data
+    DD = 0X80000006  // Current dynamic data
 }
 
 // Define the possible types of a registry value
-export enum REG{
-    SZ = 1, // String value
-    EXPAND_SZ = 2, // Expandable string value
-    BINARY = 3, // Binary data value
-    DWORD = 4, // DWORD value
-    DWORD_BIG_ENDIAN = 5, // DWORD value in big-endian format
-    DWORD_LITTLE_ENDIAN = 4, // DWORD value in little-endian format
-    LINK = 6, // Symbolic link value
-    MULTI_SZ = 7, // Multiple strings value
-    RESOURCE_LIST = 8, // Resource list value
+export enum REG {
+    SZ = 1,
+    EXPAND_SZ = 2,
+    BINARY = 3,
+    DWORD = 4,
+    DWORD_BIG_ENDIAN = 5,
+    DWORD_LITTLE_ENDIAN = 4,
+    LINK = 6,
+    MULTI_SZ = 7,
+    RESOURCE_LIST = 8
 }
 
-// Native module for registry operations
-let native: any; 
-
+let native: any;
 // Function to get the native module
-function getNative(){
-    if(!native){
-        native = require('./build/Release/registry.node')
+function getNative () {
+    if (!native) {
+        native = require('./build/Release/native.node')
     }
-    return native;
+    return native
 }
 
 // Function to get a registry key and its values
-export function getRegistryKey(root: HKEY, path: string): { [name: string]: RegistryValue } | null {
+export function getRegistryKey(root: HK, path: string): { [name: string]: RegistryValue } | null {
     let ret: { [name: string]: RegistryValue } = {};
     let key = getNative().getKey(root, path);
     if (!key) {
@@ -54,7 +51,7 @@ export function getRegistryKey(root: HKEY, path: string): { [name: string]: Regi
 }
 
 // Function to get a specific value of a registry key
-export function getRegistryValue (root: HKEY, path: string, name: string): any {
+export function getRegistryValue (root: HK, path: string, name: string): any {
     let key = getRegistryKey(root, path)
     if (!key || !key[name]) {
         return null
@@ -63,23 +60,20 @@ export function getRegistryValue (root: HKEY, path: string, name: string): any {
 }
 
 // Function to set a value of a registry key
-export function setRegistryValue (root: HKEY, path: string, name: string, type: REG.MULTI_SZ, value: string[]): any
-export function setRegistryValue (root: HKEY, path: string, name: string, type: REG, value: string): any
-export function setRegistryValue (root: HKEY, path: string, name: string, type: REG, value: string|string[]): any {
+export function setRegistryValue (root: HK, path: string, name: string, type: REG.MULTI_SZ, value: string[]): any
+export function setRegistryValue (root: HK, path: string, name: string, type: REG, value: string): any
+export function setRegistryValue (root: HK, path: string, name: string, type: REG, value: string|string[]): any {
     return getNative().setValue(root, path, type, name, value)
 }
 
-// Function to list all subkeys of a registry key
-export function listRegistrySubkeys (root: HKEY, path: string): string[] {
+export function listRegistrySubkeys (root: HK, path: string): string[] {
   return getNative().listSubkeys(root, path)
 }
 
-// Function to create a registry key
-export function createRegistryKey (root: HKEY, path: string) {
+export function createRegistryKey (root: HK, path: string) {
   return getNative().createKey(root, path)
 }
 
-// Function to delete a registry key
-export function deleteRegistryKey (root: HKEY, path: string) {
+export function deleteRegistryKey (root: HK, path: string) {
   return getNative().deleteKey(root, path)
 }
--- test.js
+++ test.js
@@ -1,55 +1,43 @@
-// Import the required modules
 var assert = require('assert');
 var mod = require('.');
 
-// Describe the test suite for the core-windows-registry module
 describe('#core-windows-registry()', () => {
-  // Define the registry key to be used in the tests
   const K = "Software\\xx-test";
-
-  // Before each test, delete and recreate the registry key
   beforeEach(() => {
     mod.deleteRegistryKey(mod.HK.CU, K);
     mod.createRegistryKey(mod.HK.CU, K);
   });
 
-  // After each test, delete the registry key
   afterEach(() => {
     mod.deleteRegistryKey(mod.HK.CU, K);
   });
 
-  // Test that keys can be created
   it('should create keys', () => {
     mod.createRegistryKey(mod.HK.CU, K + '\\test');
     assert.notEqual(mod.getRegistryKey(mod.HK.CU, K + '\\test'), null);
   });
 
-  // Test that keys can be listed
   it('should list keys', () => {
     mod.createRegistryKey(mod.HK.CU, K + '\\test');
     assert.equal(mod.listRegistrySubkeys(mod.HK.CU, K).length, 1);
     assert.equal(mod.listRegistrySubkeys(mod.HK.CU, K)[0], 'test');
   });
 
-  // Test that keys can be deleted
   it('should delete keys', () => {
     mod.deleteRegistryKey(mod.HK.CU, K);
     assert.equal(mod.getRegistryKey(mod.HK.CU, K), null);
   });
 
-  // Test that DWORD values can be written and read
   it('should write & read DWORD values', () => {
     mod.setRegistryValue(mod.HK.CU, K, 'value', mod.REG.DWORD, 123);
     assert.equal(mod.getRegistryValue(mod.HK.CU, K, "value"), 123);
   });
 
-  // Test that SZ values can be written and read
   it('should write & read SZ values', () => {
     mod.setRegistryValue(mod.HK.CU, K, 'value', mod.REG.SZ, "123");
     assert.equal(mod.getRegistryValue(mod.HK.CU, K, "value"), "123");
   });
 
-  // Test that complete keys can be read
   it('should read complete keys', () => {
     mod.setRegistryValue(mod.HK.CU, K, 'a', mod.REG.SZ, "1");
     mod.setRegistryValue(mod.HK.CU, K, 'b', mod.REG.SZ, "2");
